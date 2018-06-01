48de460044e6dff4ab450bdf702d43afd4532800
BenCharbonneau <benscharb@gmail.com>
2018-06-01 10:48:25 -0500

    Moved private key id to .env

--- databaseKey.js
+++ databaseKey.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const keyJSON = {
   "type": "service_account",
   "project_id": "an-emotion-for-the-moment",
-  "private_key_id": "e9444e357aed3103833fec420c3e43bc3164f59d",
+  "private_key_id": process.env.F_PRIVATE_KEY_ID,
   "private_key": "-----BEGIN PRIVATE KEY-----\n"+process.env.F_PRIVATE_KEY+"\n-----END PRIVATE KEY-----\n",
   "client_email": process.env.F_CLIENT_EMAIL,
   "client_id": process.env.F_CLIENT_ID,
